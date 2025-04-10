387b0b37b4a4bda19bba5b388ba11e8facee7669
user_name <user@email.com>
2025-04-10 18:48:31 -0400

    ModelAdmin done

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Publication
 
 # Register your models here.
+class PublicationAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "body")
+    search_fields = ("title", "author__username")
+    list_filter = ("author", "author__username")
+
+    admin.site.register(Publication,)
