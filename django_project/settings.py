f2661132e9777081bacdcd7909f34107703b7690
user_name <user@email.com>
2025-04-10 17:41:59 -0400

    blog app added

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-]
+    "blog", # new
+    ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
